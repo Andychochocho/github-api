9f3bde9b298b0da1a7d3647072510f381e1ca521
Andrew Cho <cho-andrew@hotmail.com>
2016-03-18 10:58:17 -0700

    add # of repos.

--- js/github-interface.js
+++ js/github-interface.js
@@ -3,23 +3,21 @@ var apiKey = require('./../.env').apiKey;
 
 $(document).ready(function() {
   $('#userInfo').click(function() {
-    event.preventDefault();
     var user = $('#username').val();
     $('#username').val("");
     $.get('https://api.github.com/users/' + user +  '?access_token=' + apiKey).then(function(response) {
       $('.showUsername').text("Username: " + response.login);
       $('.showAvatar').append('<img src=' + response.avatar_url + ">");
+      $('.showRepoNum').text('Public repository number: ' + response.public_repos)
     }).fail(function(error){
       console.log(error.responseJSON.message);
     });
 
-    // $.get('https://api.github.com/users/:' + user + "/repos").then(function(response) {
-    //   console.log(JSON.stringify(response));
-    //   $('.showRepos').text("Repos: " + response.owner);
-    // }).fail(function(error){
-    //   console.log(error.responseJSON.message);
-    // });
+    $.get('https://api.github.com/users/' + user + "/repos" + '?access_token=' + apiKey).then(function(response) {
+      $('.showRepos').text("Repos: " + response.login);
+      console.log(JSON.stringify(response.login));
+    }).fail(function(error){
+      console.log(error.responseJSON.message);
+    });
   });
 });
-
-      // $('.showRepos').text("Repos: " + response.public_repos);
