7064b52c8e923594e7c7b1d75f8188ca62f749e0
Andrew Cho <cho-andrew@hotmail.com>
2016-03-18 09:25:42 -0700

    add functionality to show user's avatar.

--- js/github-interface.js
+++ js/github-interface.js
@@ -3,13 +3,23 @@ var apiKey = require('./../.env').apiKey;
 
 $(document).ready(function() {
   $('#userInfo').click(function() {
+    event.preventDefault();
     var user = $('#username').val();
     $('#username').val("");
-    $('.showUsername').text("Username: " + user);
     $.get('https://api.github.com/users/' + user +  '?access_token=' + apiKey).then(function(response) {
-      console.log(JSON.stringify(response));
+      $('.showUsername').text("Username: " + response.login);
+      $('.showAvatar').append('<img src=' + response.avatar_url + ">");
     }).fail(function(error){
       console.log(error.responseJSON.message);
     });
+
+    // $.get('https://api.github.com/users/:' + user + "/repos").then(function(response) {
+    //   console.log(JSON.stringify(response));
+    //   $('.showRepos').text("Repos: " + response.owner);
+    // }).fail(function(error){
+    //   console.log(error.responseJSON.message);
+    // });
   });
 });
+
+      // $('.showRepos').text("Repos: " + response.public_repos);
