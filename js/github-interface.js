c55bc712adb870d627908a50ff8786cd0ecb76f1
Andrew Cho <cho-andrew@hotmail.com>
2016-03-19 07:10:35 -0700

    add functionality to see repositories on browser page.

--- js/github-interface.js
+++ js/github-interface.js
@@ -3,19 +3,21 @@ var apiKey = require('./../.env').apiKey;
 
 $(document).ready(function() {
   $('#userInfo').click(function() {
+    //user info
     var user = $('#username').val();
     $('#username').val("");
     $.get('https://api.github.com/users/' + user +  '?access_token=' + apiKey).then(function(response) {
       $('.showUsername').text("Username: " + response.login);
       $('.showAvatar').append('<img src=' + response.avatar_url + ">");
-      $('.showRepoNum').text('Public repository number: ' + response.public_repos)
+      $('.showRepoNum').text('Public repository number: ' + response.public_repos);
     }).fail(function(error){
       console.log(error.responseJSON.message);
     });
-
-    $.get('https://api.github.com/users/' + user + "/repos" + '?access_token=' + apiKey).then(function(response) {
-      $('.showRepos').text("Repos: " + response.login);
-      console.log(JSON.stringify(response.login));
+    //list all repositories
+    $.get('https://api.github.com/users/' + user + "/repos?access_token=" + apiKey).then(function(response) {
+    for (var i = 0; i < response.length; i++) {
+      $('.showRepos').append(response[i].name + "<br>");
+    }
     }).fail(function(error){
       console.log(error.responseJSON.message);
     });
